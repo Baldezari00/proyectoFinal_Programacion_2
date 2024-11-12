a370b5efa7cbb333ef1f9fb465676bfb949cb6e3
Matias Gonzalez <matiaslg2000@gmail.com>
2024-11-12 14:25:33 -0300

    Hice las clases del tipoJugador y su interfaz

--- src/main/java/com/example/proyectofinalprogii/Usuario/Manejo_Usuario/Personaje.java
+++ src/main/java/com/example/proyectofinalprogii/Usuario/Manejo_Usuario/Personaje.java
@@ -1,9 +1,48 @@
 package com.example.proyectofinalprogii.Usuario.Manejo_Usuario;
 
-public class Personaje {
+public abstract class Personaje {
     // TRABAJO DE MATI:
     // crear los 3 personajes necesarios junto con clase padre (esta misma), sus atributos y demás.
     // 1 interfaz que la utilicen 2 de los personajes
-    // extra: serializar personajes (convertir personaje a JSON y viceversa. Sin archivo y dentro de la misma clase).
+    // extra: seri  alizar personajes (convertir personaje a JSON y viceversa. Sin archivo y dentro de la misma clase).
+    private static int contarId = 0;
+    private int ID;
+    private String Nombre;
+    protected int vida;
 
+    public Personaje(int ID, String nombre, int vida) {
+        this.ID = contarId++;
+        this.Nombre = nombre;
+        this.vida = vida;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+
+
+    public abstract String Ataque();
+    public abstract String RecibirDanio(int danio);
+
+    @Override
+    public String toString() {
+        return "Personaje{" +
+                "ID=" + ID +
+                ", Nombre='" + Nombre + '\'' +
+                ", vida=" + vida +
+                ' ';
+    }
 }
