aaeaf70b5b20f1492a3a5ad851a8a50bb51823c9
fedegianera <federicogianera70@gmail.com>
2024-11-12 14:37:48 -0300

    agregue las funciones de guardar en json los jugadores y traerlos

--- src/main/java/com/example/proyectofinalprogii/OperacionesBasicasJSON/OperacionLecturaEscritura.java
+++ src/main/java/com/example/proyectofinalprogii/OperacionesBasicasJSON/OperacionLecturaEscritura.java
@@ -8,6 +8,13 @@ import com.example.proyectofinalprogii.Usuario.Mochila.Objeto;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
 
 public class OperacionLecturaEscritura {
     /**
@@ -15,8 +22,63 @@ public class OperacionLecturaEscritura {
      Esta clase tiene los metodos estaticos para guardar jugadores o traerlos de archivos json
      **/
 
+    public static void jugadoresToArchivo(Map<String, Jugador> jugadores) {
+         String archivoJugadores = "jugadores.json";
 
+         try (FileWriter file = new FileWriter(archivoJugadores)) {
+              JSONArray jsonArray = new JSONArray();
 
+              for (Map.Entry<String, Jugador> entry : jugadores.entrySet()) {
+                   Jugador jugadorAux = entry.getValue();
 
-    //mas tarde hago de json al jugador y las funciones de crear cuenta e iniciar sesion
+                   JSONObject jugadorJson = ManejoJugador.jugadorToJson(jugadorAux);
+
+                   jsonArray.put(jugadorJson);
+              }
+
+              file.write(jsonArray.toString(4));
+
+              System.out.println("Jugadores guardados en archivo");
+         } catch (IOException e) {
+              e.printStackTrace();
+         }
+    }
+
+     public static void archivoToJugadores(Map<String, Jugador> mapJugadores) {
+         String archivo = "jugadores.json";
+
+         try {
+              JSONArray arrJsonJugadores = new JSONArray(leerArchivo(archivo));
+
+               cargarMapJugadores(arrJsonJugadores, mapJugadores);
+
+         } catch (Exception e) {
+              e.printStackTrace();
+         }
+     }
+
+     public void cargarMapJugadores(JSONArray arrJsonJugadores, Map<String, Jugador> mapJugadores) {
+          mapJugadores.clear();
+
+          for (int i = 0; i < arrJsonJugadores.length(); i++) {
+               JSONObject jsonJugador = arrJsonJugadores.getJSONObject(i);
+
+               Jugador jugador = ManejoJugador.jsonToJugador(jsonJugador);
+               mapJugadores.put(jugador.getNombreUsuario(), jugador);
+          }
+     }
+
+     public static JSONTokener leerArchivo(String archivo) {
+          JSONTokener tokener = null;
+
+          try {
+               tokener = new JSONTokener(new FileReader(archivo));
+          } catch (FileNotFoundException e) {
+               e.printStackTrace();
+          }
+
+          return tokener;
+     }
+
+    //mas tarde hago las funciones de crear cuenta e iniciar sesion
 }
